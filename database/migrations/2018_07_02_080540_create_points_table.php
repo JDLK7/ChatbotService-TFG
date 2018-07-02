f00e07c12598354d088f3e4cf32d47f35005b15f
Jose Domenech Leal <josesguay@gmail.com>
2018-07-02 10:43:42 +0200

    Login y registro con UUIDs

---
+++ database/migrations/2018_07_02_080540_create_points_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->boolean('shouldExist')->default(0);
+            $table->boolean('exists')->default(0);
+            $table->jsonb('properties')->nullable();
+            $table->timestamps();
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+}
