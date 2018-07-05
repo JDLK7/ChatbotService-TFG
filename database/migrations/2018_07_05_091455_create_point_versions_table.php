e82225b8f2705bd567add355ed97bdf246bc46e8
Jose Domenech Leal <josesguay@gmail.com>
2018-07-05 11:25:20 +0200

    Migraciones soportan versiones de punto

---
+++ database/migrations/2018_07_05_091455_create_point_versions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePointVersionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('point_versions', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->boolean('shouldExist')->default(true);
+            $table->boolean('exists')->default(false);
+            $table->jsonb('properties')->nullable();
+            $table->uuid('point_id');
+            $table->uuid('user_id');
+            $table->timestamps();
+
+            $table->primary('id');
+            $table->foreign('point_id')->references('id')->on('points')
+                ->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('point_versions');
+    }
+}
