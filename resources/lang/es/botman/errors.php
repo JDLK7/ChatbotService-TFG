8996753bfce5d636576074f21ed1a5739b3e894d
Jose Domenech Leal <josesguay@gmail.com>
2018-07-10 17:24:13 +0200

    Ficheros de traducción para preguntas del chatbot

---
+++ resources/lang/es/botman/errors.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Botman errors
+    |--------------------------------------------------------------------------
+    |
+    | //
+    |
+    */
+
+    'required_type' => 'El campo "type" es obligatorio',
+    'unknown_type' => 'El tipo :type es un tipo de punto desconocido',
+
+];
