85b63bc13023119550f4f3eace9e156c3e0b4576
Jose Domenech Leal <josesguay@gmail.com>
2018-07-19 11:26:48 +0200

    Obtención del punto más cercano
    Se puede obtener el punto más cercano a unas determinadas coordenadas y dentro de una distancia máxima.

--- app/Http/Controllers/PointController.php
+++ app/Http/Controllers/PointController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Point;
 use Illuminate\Http\Request;
 use App\Services\PointService;
 use Illuminate\Support\Facades\Validator;
@@ -28,6 +29,43 @@ class PointController extends Controller
 
         return response()->json([
             'success' => true,
+        ]);
+    }
+
+    /**
+     * A partir de las coordenadas dadas obtiene el punto más 
+     * cercano si se encuentra dentro de la distancia máxima.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getNearestPoint(Request $request) {
+        $request->validate([
+            'lat' => 'required|numeric',
+            'lng' => 'required|numeric',
+            'threshold' => 'nullable|numeric',
+        ]);
+
+        $threshold = $request->query('threshold', 10);
+        $currentPosition = new Point([
+            'latitude' => $request->query('lat'),
+            'longitude' => $request->query('lng'),
+        ]);
+
+        $nearest = $currentPosition->nearestPoint();
+        
+        if ($currentPosition->distanceTo($nearest) > $threshold) {
+            return response()->json([
+                'success' => true,
+                'message' => 'No hay ningún punto cercano',
+                'point' => null,
+            ]);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Punto cercano encontrado',
+            'point' => $nearest,
         ]);
     }
 }
