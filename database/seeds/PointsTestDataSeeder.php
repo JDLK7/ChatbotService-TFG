ae77d0b74d12f07f90827fd8b632509edb3deb3b
Jose Domenech Leal <josesguay@gmail.com>
2018-08-26 10:08:31 +0200

    Seeder para generar datos de prueba mejorado

--- database/seeds/PointsTestDataSeeder.php
+++ database/seeds/PointsTestDataSeeder.php
@@ -26,6 +26,8 @@ class PointsTestDataSeeder extends Seeder
             $date = Carbon::now()->month(rand(1, 12));
 
             $point = factory(CrosswalkPoint::class)->create([
+                'latitude' => mt_rand(38381494, 38390400) / 1000000,
+                'longitude' => mt_rand(508847, 518095) * (-1) / 1000000,
                 'created_at' => $date,
                 'updated_at' => $date,
             ]);
@@ -35,16 +37,16 @@ class PointsTestDataSeeder extends Seeder
         for ($i = 1; $i < $currentMonth; $i++) {
             $pointsCount = Point::whereMonth('created_at', $i)->count();
             $points = Point::whereMonth('created_at', $i)
-                ->take(rand(0, $pointsCount))->get();
+                ->take(mt_rand(0, $pointsCount))->get();
 
             foreach ($points as $point) {
                 $revision = $point->makeVersion($fakeUser);
                 $revision->created_at = $point->created_at;
                 $revision->updated_at = $point->updated_at;
                 $revision->properties = (object) [
-                    'hasCurbRamps' => rand(0, 1) == 1,
-                    'visibility' => $visibilityGrades[rand(0, 2)],
-                    'hasSemaphore' => rand(0, 1) == 1,
+                    'hasCurbRamps' => mt_rand(0, 1) == 1,
+                    'visibility' => $visibilityGrades[mt_rand(0, 2)],
+                    'hasSemaphore' => mt_rand(0, 1) == 1,
                 ];
                 $revision->save();
             }
