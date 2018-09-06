7c649d01de1e9b414aac00cd3c6a42e949b6690d
Jose Domenech Leal <josesguay@gmail.com>
2018-09-06 13:13:29 +0200

    Añadidos archivos de traducción en inglés

---
+++ resources/lang/en/botman/questions.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Botman questions
+    |--------------------------------------------------------------------------
+    |
+    | Preguntas para las conversaciones.
+    |
+    */
+
+    'fallback' => 'Error: Unable to ask question.',
+    'appreciation' => 'Thanks for your colaboration',
+    'ask_existence' => 'Is there a :type in your current location?',
+    'ask_obstacle_type' => 'What type of obstacle do you think it is?',
+    'ask_point_type' => 'What type of point do you want to create?',
+    'ask_accessibility_rating' => 'Rate the accessibility grade of this :type',
+    'ask_crosswalk_curb_ramps' => 'Does this crosswalk have curb ramps to climb into the sidewalk?',
+    'ask_crosswalk_visibility' => 'How good is the visibility from the sidewalk to the road?',
+    'ask_crosswalk_semaphore' => 'Does this crosswalk have acoustic semaphores?',
+];
---
+++ resources/lang/en/points/types.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tipos de puntos
+    |--------------------------------------------------------------------------
+    |
+    | Tipos de puntos disponibles en la plataforma.
+    |
+    */
+
+    'crosswalk' => 'crosswalk',
+    'urbanFurniture' => 'urban furniture',
+    'works' => 'works',
+    'obstacle' => 'obstacle'
+];
